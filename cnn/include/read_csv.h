959cd57997b6dd9bec801b74c8be98c584289720
Ge777777 <getianqi@stu.scu.edu.cn>
2023-05-23 15:34:48 +0800

    read

--- cnn/include/read_csv.h
+++ cnn/include/read_csv.h
@@ -1,14 +1,16 @@
 #pragma once
-
-#include"tensor.h"
-#include<vector>
-#include<string.h>
-
-struct case_s
-{
-    tensor<double> data,dataout;
+#include "tensor.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+struct case_s{
+    tensor<double> data;
+    tensor<double> dataout;
+    case_s(int x,int y,int z,int x1,int y1,int z1):data(x,y,z),dataout(x1,y1,z1){};
 };
 
-std::vector<case_s> read_csv();
+std::vector<case_s> read_csv(const std::string& filename);
 
 void pre_read(std::string path);
