3ec22bfafe8016a935abe821b3fbabaeff05fa8c
Ge777777 <getianqi@stu.scu.edu.cn>
2023-05-23 22:53:11 +0800

    add a constructor

--- cnn/include/read_csv.h
+++ cnn/include/read_csv.h
@@ -2,11 +2,15 @@
 
 #include"tensor.h"
 #include<vector>
-#include<string.h>
+#include<cstring>
+#include<fstream>
+#include<iostream>
+#include<sstream>
 
 struct case_s
 {
     tensor<double> data,dataout;
+    case_s(int x,int y,int z,int outx,int outy,int outz):data(x,y,z),dataout(outx,outy,outz){}
 };
 
 std::vector<case_s> read_csv();
