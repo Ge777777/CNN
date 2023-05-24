997dc4e7f832d27711405b5eaefcac3d1d8dd2d7
Ge777777 <getianqi@stu.scu.edu.cn>
2023-05-24 14:45:20 +0800

    一些小修正

--- cnn/include/read_csv.h
+++ cnn/include/read_csv.h
@@ -7,11 +7,25 @@
 #include<iostream>
 #include<sstream>
 
+const int header = 1;
+
+const int in_x = 28;
+
+const int in_y = 28;
+
+const int in_z = 1;
+
+const int out_x = 10;
+
+const int out_y = 1;
+
+const int out_z = 1;
+
 struct case_s
 {
-    tensor<double> data,dataout;
+    tensor<int> data,dataout;
     case_s(int x,int y,int z,int outx,int outy,int outz):data(x,y,z),dataout(outx,outy,outz){}
 };
 
-std::vector<case_s> read_csv(std::string filename = "../dataset/mnist_test.csv");
+std::vector<case_s> read_csv(std::string filename = "../dataset/test.csv");
 
--- cnn/src/read_csv.cpp
+++ cnn/src/read_csv.cpp
@@ -1,38 +1,41 @@
 #include "../include/read_csv.h"
 
-std::vector<case_s> read_csv (std::string path){
+std::vector<case_s> read_csv(std::string path) {
     std::vector<case_s> dataset;
-    case_s row(28,28,1,10,1,1);
-    std::ifstream file (path);
-    if (!file.is_open()){
+    case_s row(in_x, in_y, in_z, out_x, out_y, out_z);
+    std::ifstream file(path);
+    if (!file.is_open()) {
         std::cout << "File not found!" << std::endl;
         return dataset;
     } else {
-        std::cout << "OK" << std::endl;
+        std::cout << "Success" << std::endl;
     }
     std::string line;
-    if(getline(file, line)) //跳过第一行(目录行，没有意义）
-    {
 
-        while (std::getline(file, line)) {
-            std::stringstream ss(line);
-            std::string str;
-            if(getline(ss,str, ',')) //每行的第一个数据为dataout
-            {
-               for(int i = 0; i < 10; i++){
-                     row.dataout(0,0,i) = 0;
-               }
-               row.dataout(0,0,std::stoi(str)) = 1;
-            }
-            //剩余的数据
-            int i = 0;
-            while (std::getline(ss, str, ',')) {
-                row.data[i] = std::stod(str);
-                i++;
-            }
-            dataset.push_back(row);
-        }
-        file.close();
+    for(int i = 0; i < header; i++) {
+        std::getline(file, line);
     }
+
+    while (std::getline(file, line)) {
+        std::stringstream ss(line);
+        std::string str;
+        if (getline(ss, str, ',')) //每行的第一个数据为 dataout
+        {
+            for (int i = 0; i < 10; i++) {
+                row.dataout(0, 0, i) = 0;
+            }
+            row.dataout(0, 0, std::stoi(str)) = 1;
+        }
+        //剩余的数据
+        for (int i = 0; i < 28*28 - 1; i++){
+            std::getline(ss, str, ',');
+            row.data[i] = std::stoi(str);
+        }
+        std::getline(ss, str, '\n');
+        row.data[28*28 - 1] = std::stoi(str);
+        dataset.push_back(row);
+    }
+
+    file.close();
     return dataset;
 }
